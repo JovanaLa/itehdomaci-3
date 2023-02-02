274965ea0fcf852875103587a7ab3fe635475566
Jovana Lazarevic <jovanalazarevic394@gmail.com>
2023-02-02 09:48:33 +0100

    Napravljen  i dodat HomeSection

---
+++ bioskop/src/components/HomeSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import './HomeSection.css';
+
+function HomeSection() {
+  return (
+    <div className='home-container'>
+      <image src='../assets/bioskop.png'/>
+      <h1>Bioskop</h1>
+      <p>Rezerviši svoja mesta</p>
+    </div>
+  );
+}
+
+export default HomeSection;
