4151331068d2ca4adc5f57f8c941aa4dc0e79976
Jovana <jovanalazarevic394@gmail.com>
2023-02-10 17:52:36 +0100

    Finalne izmene

--- bioskop/src/components/Cards.js
+++ bioskop/src/components/Cards.js
@@ -32,34 +32,8 @@ function Cards({data1, data2, type}) {
   );
         }
 
-  function Cards2({data3, data4, type}) {
 
-
-    return (
-      <div className='cards'>
-        <h1>Repertoar</h1>
-        <div className='cards__container'>
-          <div className='cards__wrapper'>
-            <ul className='cards__items'>
-            {
-               data3.map(item => (<CardItem obj={item} type={type} />))
-             }
-  </ul>
-  {
-              data4!= null ? 
-              <ul className='cards__items'>
-                {
-                  data4.map(item => (<CardItem obj={item} type={type} />))
-                }
-             </ul> : <></>
-            }
-          
-          </div>
-        </div>
-      </div>
       
-    );
-}
 
 export default Cards;
 
